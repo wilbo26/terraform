99ab2dd7be43b10253a95d1c78580e0761cb7338
Will Box <william.box@insightglobal.com>
2019-05-29 16:38:59 -0400

    Working v0.0.1 for building test VM in lab

--- projects/vsphere/vsphere.tf
+++ projects/vsphere/vsphere.tf
@@ -1,3 +1,4 @@
+#Define our vsphere provider and pull from our local login tfvar file
 provider "vsphere" {
   user           = "${var.vsphere_user}"
   password       = "${var.vsphere_password}"
@@ -7,14 +8,35 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+#Define data sources for deploying VM to the DCX test lab.
+data "vsphere_datacenter" "dc" {
+  name = "DCX"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "DCX-Test"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "LAB"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+#Resource definition to build our test VM
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
   num_cpus = 2
   memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  guest_id = "centos7_64Guest"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
