6728b83ab0259939e16dc9491567e0727237348a
Will Box <william.box@insightglobal.com>
2019-07-09 21:36:29 -0400

    added a few remote exec packages for testing and some TOTO notes

--- example.tf
+++ example.tf
@@ -10,7 +10,11 @@ resource "aws_instance" "example" {
   key_name      = "AWS_XPS_LIN"
 
   provisioner "remote-exec" {
-    inline = ["sudo apt install ansible -y"]
+    inline = [
+                "sudo apt install ansible -y",
+                "sudo apt install htop -y",
+                "sudo apt install iotop -y"
+             ]
 
     connection {
       host        = "${self.public_ip}"
@@ -24,3 +28,7 @@ resource "aws_instance" "example" {
 resource "aws_eip" "ip" {
   instance = aws_instance.example.id
 }
+
+#TODO: Add local-exec to run local playbook against the new host once ansible is installed remotely
+#TODO: Not sure how this will work, but seems reasonable. 
+#TODO: https://alex.dzyoba.com/blog/terraform-ansible/ for reference. 
